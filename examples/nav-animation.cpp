8e91749ebe4b1a98d2349863422be48e0e86f6ce
Karl Yerkes <karl.yerkes@gmail.com>
2023-02-02 05:02:07 -0800

    use cone

--- examples/nav-animation.cpp
+++ examples/nav-animation.cpp
@@ -4,6 +4,7 @@
 //
 #include "al/app/al_App.hpp"
 #include "al/app/al_GUIDomain.hpp"
+#include "al/graphics/al_Shapes.hpp"
 #include "al/math/al_Random.hpp"
 
 using namespace al;
@@ -11,10 +12,12 @@ using namespace al;
 double r() { return rnd::uniformS(); }
 
 struct MyApp : App {
-  Nav agent;
-  Nav target;
+  Nav preditor;
+  Nav prey;
   double time{0};
   double angle{0};
+  Light light;
+  Material material;
 
   Mesh mesh;
 
@@ -29,23 +32,27 @@ struct MyApp : App {
     // nav().faceToward(Vec3d(0), Vec3d(0), 0);
     // which has no effect because of the final 0!
 
-    // create a prototype agent body
+    // create a prototype preditor body
+    addCone(mesh);
+    mesh.generateNormals();
+    /*
     mesh.primitive(Mesh::TRIANGLE_FAN);
     mesh.vertex(0, 0, -2);
     mesh.color(0, 0, 0);
-    mesh.vertex(0, 1, 0);
+    mesh.vertex(0, 1, 0); // !
     mesh.color(1, 0, 0);
     mesh.vertex(-1, 0, 0);
     mesh.color(0, 1, 0);
     mesh.vertex(1, 0, 0);
     mesh.color(0, 0, 1);
-    mesh.vertex(0, 1, 0);
+    mesh.vertex(0, 1, 0); // !
     mesh.color(1, 0, 0);
+    */
   }
 
   void randomize() {
-    target.pos(r(), r(), r());
-    target.quat().set(r(), r(), r(), r()).normalize();
+    prey.pos(r(), r(), r());
+    prey.quat().set(r(), r(), r(), r()).normalize();
   }
 
   void onAnimate(double dt) override {
@@ -56,19 +63,19 @@ struct MyApp : App {
     time += dt;
     angle += 0.1;
 
-    // The target (blue) aims for the origin (0,0,0) while the agent (red) aims
-    // for the target. moveF(t) means "move forward" by amount t. faceToward
+    // The prey (blue) aims for the origin (0,0,0) while the preditor (red) aims
+    // for the prey. moveF(t) means "move forward" by amount t. faceToward
     // also accepts an amount t that controls the rate of steering.
-    target.faceToward(Vec3d(0, 0, 0), Vec3d(0, 1, 0), 0.03);
-    target.moveF(0.2);
-    agent.faceToward(target, Vec3d(0, 1, 0), 0.03);
-    agent.moveF(0.2);
-    // agent.nudgeToward(target, -0.01);
+    prey.faceToward(Vec3d(0, 0, 0), Vec3d(0, 1, 0), 0.05);
+    prey.moveF(0.3);
+    preditor.faceToward(prey, Vec3d(0, 1, 0), 0.03);
+    preditor.moveF(0.2);
+    // preditor.nudgeToward(prey, -0.01);
 
     // XXX we have to step each Nav to get them to move and turn!!!
     // skip this and nothing will move.
-    target.step(dt);
-    agent.step(dt);
+    prey.step(dt);
+    preditor.step(dt);
 
     // XXX there's an exception. The App's nav() automatically steps. Don't call
     // nav().step() because that is done automatically.
@@ -78,6 +85,7 @@ struct MyApp : App {
     // camera based on what is happening in the virtual world. We could make
     // cinematic maneuvers such as a "dolly shot" or mimic styles such as Dutch
     // angle or "handheld". Cuts and fades are also possible.
+    // nav() = preditor;
   }
 
   void onDraw(Graphics& g) override {
@@ -87,6 +95,11 @@ struct MyApp : App {
     g.pointSize(10);
 
     g.rotate(angle, Vec3d(0, 1, 0));
+
+    g.depthTesting(true);
+    g.lighting(true);
+    g.light(light);
+    g.material(material);
 
     {
       Mesh mesh(Mesh::LINES);
@@ -102,9 +115,9 @@ struct MyApp : App {
       g.draw(mesh);
     }
 
-    // draw a body for each agent
+    // draw a body for each preditor
     {
-      Nav& a(agent);
+      Nav& a(preditor);
       g.pushMatrix();  // push()
       g.translate(a.pos());
       g.rotate(a.quat());  // rotate using the quat
@@ -113,7 +126,7 @@ struct MyApp : App {
       g.popMatrix();  // pop()
     }
     {
-      Nav& a(target);
+      Nav& a(prey);
       g.pushMatrix();  // push()
       g.translate(a.pos());
       g.rotate(a.quat());  // rotate using the quat
